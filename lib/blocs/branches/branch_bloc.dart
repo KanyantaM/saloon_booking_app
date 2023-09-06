0d7d656b6b414d550d15cce911ea0b8295ac879a
KanyantaM <kanyanta.1makasa@gmail.com>
2023-09-06 04:48:21 +0200

    using the on<> formate for the location bloc

--- lib/blocs/branches/branch_bloc.dart
+++ lib/blocs/branches/branch_bloc.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 import 'package:beautonomi/blocs/branches/branch_events.dart';
 import 'package:beautonomi/blocs/branches/branch_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -9,17 +7,27 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import '../../utilites/location/finding_user.dart';
 
 class BranchBloc extends Bloc<BranchEvent, BranchState> {
-  BranchBloc() : super(LocationsInitialState());
-
-
-  Stream<BranchState> mapEventToState(BranchEvent event) async* {
-    if (event is SetLocationsEvent) {
-      if (event.useDeviceLocation) {
+  BranchBloc() : super(LocationsInitialState()){
+    on<SetLocationsEvent>((event, emit)async{
+      if(event.useDeviceLocation){
         Position? position = await determineLastPostion();
         LatLng currentLocation = LatLng(position!.latitude, position.longitude);
-        yield LocationLoadedState(userLocation:currentLocation, destination: event.branchLocation, destinationName:event.branchName);
-      } else {
+        emit(LocationLoadedState(userLocation:currentLocation, destination: event.branchLocation, destinationName:event.branchName,));
+      }else{
         //TODO: allow the user ot manually find their location
       }
-    }   }
+    });
+  }
+
+
+  // Stream<BranchState> mapEventToState(BranchEvent event) async* {
+  //   if (event is SetLocationsEvent) {
+  //     if (event.useDeviceLocation) {
+  //       Position? position = await determineLastPostion();
+  //       LatLng currentLocation = LatLng(position!.latitude, position.longitude);
+  //       yield LocationLoadedState(userLocation:currentLocation, destination: event.branchLocation, destinationName:event.branchName);
+  //     } else {
+  //       //TODO: allow the user ot manually find their location
+  //     }
+  //   }   }
 }
